672ee00511f0987f53d1060190d08888d2fc96e4
sadsirko <den673883@gmail.com>
2020-01-02 23:23:02 +0200

    we can kill)))

--- chess.js
+++ chess.js
@@ -19,6 +19,7 @@ let cell = { };
  let zone = [];
  let choosen_cell_now = 0, choosen_cell_prev = null;
  let spec_arr = [];
+let flag = 1;
 //for desk cells
 
 function mouseMoveHandlerX(e) {
@@ -118,41 +119,7 @@ for(let x_cell = 0; x_cell < canvas.width; x_cell += 2 * cell.width ){
 }
 }
 
-// black chekers
-/*function draw_b_chekers_first(){
-for(let num  = 0; num < 3; num ++ ){
-  black[num] = [];
-        for(let n  = 0; n < 4; n ++ ){           
-             if( num != 1 ) black [num][n] = { x : 90 + n * 120, y : 30 + num * 60  };
-             else black [num][n] = { x : 30 + n * 120, y : 30 + num * 60  }    
-        drawcheker_b(black[num][n].x,black[num][n].y);
-        } 
-}
-}
 
-
-function draw_w_chekers_first(){
-for(let num  = 0; num < 3; num ++ ){
-  white[num] = [];
-        for(let n  = 0; n < 4; n ++ ){    
-
-             if( num == 1 ) white [num][n] = { x : 90 + n * 120, y : 330 + num * 60  };
-             else white [num][n] = { x : 30 + n * 120, y : 330 + num * 60  }    
-        drawcheker_w(white[num][n].x,white[num][n].y);
-        } 
-}
-}
-
-
-/*function draw_chekers(){
-for(let num  = 0; num < 3; num ++ ){
-        for(let n  = 0; n < 4; n ++ ){           
-        drawcheker_w(white[num][n].x,white[num][n].y);
-        drawcheker_b(black[num][n].x,black[num][n].y);
-        } 
-}
-}
-*/
 function draw_chekers(){
   for(let j = 0 ; j < 4 ; j++)
   {
@@ -204,27 +171,148 @@ if(choosen_cell_now != buffer) buffer = choosen_cell_now;
 }
 
 
-function move_on_60(){
-  if (choosen_cell_now - choosen_cell_prev == 4){
+function move_w(now,pre){
+     let c = rules_1(now,pre,'white');
+     let a = find_spec_ar(now).cheker;
+     let b = find_spec_ar(pre).cheker; 
+     let d = rules_2(now,pre);
+console.log( c );
+  if(b == 'w' && a == 'null' && c) { 
+  find_spec_ar(now).cheker = 'w';
+find_spec_ar(pre).cheker = 'null';
+}
+ if(b == 'w' && a == 'null'  && d) { 
+  find_spec_ar(now).cheker = 'w';
+find_spec_ar(pre).cheker = 'null';
+}
 
-                         
-  }
 }
 
 
-function find_choosed_cheker_on_board(nn)
-{
-  for(let j = 0 ; j < 4 ; j++)
+
+function move_b(now,pre){
+  let a = find_spec_ar(now).cheker;
+  let b = find_spec_ar(pre).cheker;
+   let c = rules_1(now,pre,'black');
+    let d = rules_2(now,pre);
+        
+ 
+  if(a == 'null' && b == 'b' &&  c) { 
+find_spec_ar(now).cheker = 'b';
+find_spec_ar(pre).cheker = 'null';
+}
+
+  if(a == 'null' && b == 'b' && d) { 
+//    console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+find_spec_ar(now).cheker = 'b';
+find_spec_ar(pre).cheker = 'null';
+}
+}
+
+
+function rules_1(now,pre,color){
+  let a1,a2,b1,b2;
+ for(let j = 0 ; j < 4 ; j++)
     {
     for (let i = 0; i < 4; i++) 
       { 
         let a = 0 + i + j ;
       let b = 4 + i - j;
-      if(spec_arr[a][b].num == nn) return spec_arr[a][b].cheker;
-      if(spec_arr[a][b - 1].num == nn) return spec_arr[a][b - 1].cheker;
+      if(spec_arr[a][b].num == now) {
+       a1 = a;
+       b1 = b;
+      }
+      if(spec_arr[a][b - 1].num == now) {
+      a1 = a;
+      b1 = b-1;
+      }
+      if(spec_arr[a][b].num == pre) {
+       a2 = a;
+       b2 = b;
+
+      }
+      if(spec_arr[a][b - 1].num == pre) {
+        a2 = a;
+       b2 = b-1;
+      }
+     } 
+    } 
+    if (color == 'white'){
+    if (( (a2 - a1) == 1) && ((b1 - b2) == 0)) return true;
+    if (( (a1 - a2) == 0) && ((b1 - b2) == 1)) return true;
+    
+    }
+    if(color == 'black'){
+    if (( (a1 - a2) == 1) && ((b1 - b2) == 0)) return true;
+    if (( (a1 - a2) == 0) && ((b2 - b1) == 1)) return true;
+    return false;
+  }
+
+}
+function rules_2(now,pre){
+  let a1,a2,b1,b2;       
+ for(let j = 0 ; j < 4 ; j++)
+    {
+    for (let i = 0; i < 4; i++) 
+      { 
+        let a = 0 + i + j ;
+      let b = 4 + i - j;
+      if(spec_arr[a][b].num == now) {
+       a1 = a;
+       b1 = b;
+      }
+      if(spec_arr[a][b - 1].num == now) {
+      a1 = a;
+      b1 = b-1;
+      }
+      if(spec_arr[a][b].num == pre) {
+       a2 = a;
+       b2 = b;
+
+      }
+      if(spec_arr[a][b - 1].num == pre) {
+        a2 = a;
+       b2 = b-1;
+      }
+     } 
+    } 
+    
+    if (( (a2 - a1) == 2) && ((b1 - b2) == 0) && spec_arr[a2-1][b1].cheker != 'null') {
+     spec_arr[a2-1][b1].cheker = 'null';
+      return true;
+    }
+    if (( (a1 - a2) == 0) && ((b1 - b2) == 2) && spec_arr[a1][b2+1].cheker != 'null') {
+    spec_arr[a1][b2+1].cheker = 'null';
+      return true;
+  }  
+    if (( (a1 - a2) == 2) && ((b1 - b2) == 0) && spec_arr[a2+1][b1].cheker != 'null') {
+    spec_arr[a2+1][b1].cheker = 'null';
+      return true;
+    }
+    if (( (a1 - a2) == 0) && ((b2 - b1) == 2) && spec_arr[a1][b2-1].cheker != 'null') {
+    spec_arr[a1][b2-1].cheker = 'null'
+      return true;
+    }
+    return false;
+  
+
+}
+
+function find_spec_ar(nn){
+   for(let j = 0 ; j < 4 ; j++)
+    {
+    for (let i = 0; i < 4; i++) 
+      { 
+        let a = 0 + i + j ;
+      let b = 4 + i - j;
+      if(spec_arr[a][b].num == nn) return spec_arr[a][b];
+      if(spec_arr[a][b - 1].num == nn) return spec_arr[a][b - 1];
      } 
     }
+        
 }
+
+
 
 
 //draw_b_chekers_first();
@@ -237,14 +325,24 @@ function draw(){
  draw_desk();
  draw_chekers();
  find_choosed_cell();   
+
 if(click){
+  if (find_spec_ar(choosen_cell_prev).cheker == 'w' && flag) {
+    move_w(choosen_cell_now,choosen_cell_prev); 
+    if(find_spec_ar(choosen_cell_prev).cheker == 'null' ) flag = 0;
+  }
+if(find_spec_ar(choosen_cell_prev).cheker == 'b' && !flag){
+move_b(choosen_cell_now,choosen_cell_prev); 
+  if(find_spec_ar(choosen_cell_prev).cheker == 'null' ) flag = 1;
+}
+
  console.log("now : " + choosen_cell_now);
  console.log("pre : " + choosen_cell_prev);
- console.log(find_choosed_cheker_on_board(choosen_cell_now));
- console.log(spec_arr);
+ console.log(find_spec_ar(choosen_cell_now));
 }
+
 }
 
 //console.log(spec_arr[6][3] );
 setInterval(draw, 100);
-console.log(spec_arr);
+console.log();
