fdedc2c91ef32b0119148bd0c7cd4f0a00d91c9e
sadsirko <den673883@gmail.com>
2020-06-06 07:39:26 +0300

    made eslint

--- server.js
+++ server.js
@@ -3,58 +3,37 @@
 const fs = require('fs');
 const http = require('http');
 const Websocket = require('websocket').server;
-const index = fs.readFileSync('./indexServ.html', 'utf8');
+const index = fs.readFileSync('./index.html', 'utf8');
 const clients = [];
-const specArr = [];
+//const specArr = [];
 
 const server = http.createServer((req, res) => {
   res.writeHead(200);
   res.end(index);
 });
 
-const workWithSpecArr = () => 
-{
-let n = 1;
-for (let i = 0; i < 7; i++) 
-{
-  specArr[i] = [];
-  for (let j = 0; j < 8; j++) 
-  {
-    specArr[i][j] = 0;
-  }
-}
 
-for (let j = 0; j < 4; j++) {
-  for (let i = 0; i < 4; i++) {
-    const a = 0 + i + j;
-    const b = 4 + i - j;
-    specArr[a ][b] = { num: n, choose: false, cheker: 'null', x: 60 + 120 * i, y: 0 +  120 * j, a, b };
-    specArr[a ][b - 1] = { num: n + 4, choose: false, cheker: 'null', x: 0 + 120 * i, y: 60 +  120 * j, a, b: b - 1  };
-    n++;
-  } n += 4;
-}
-}
 
-server.listen(8000, () => {
-  console.log('Listen port 8000');
+server.listen(8080, () => {
+  console.log('Listen port 8080');
 });
 
 const ws = new Websocket({
   httpServer: server,
   autoAcceptConnections: false
-});  
+});
 
 
 
-ws.on('message', (message) => {
+ws.on('message', message => {
 
-    //log the received message and send it back to the client
-    console.log('received: ', message);
-    ws.send(`Hello, you sent -> ${message}`)
-})
+  //log the received message and send it back to the client
+  console.log('received: ', message);
+  ws.send(`Hello, you sent -> ${message}`);
+});
 
 ws.on('request', req => {
-  workWithSpecArr();
+  // workWithSpecArr();
 
 
   const connection = req.accept('', req.origin);
@@ -63,10 +42,10 @@ ws.on('request', req => {
   connection.on('message', message => {
     const dataName = message.type + 'Data';
     const data = message[dataName];
-    const obj = JSON.parse(data);
-    console.dir(`X: ${obj.x}, Y : ${obj.y}`);
-    console.dir(message);
-    console.log('Received: ' + data);
+    // const obj = JSON.parse(data);
+    //  console.dir(`X: ${obj.x}, Y : ${obj.y}`);
+    //   console.dir(message);
+    // console.log('Received: ' + data);
     clients.forEach(client => {
       if (connection !== client) {
         client.send(data);
